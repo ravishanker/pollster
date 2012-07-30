25623234ab1f25933ee9f9e603a1ecccd573a274
Ravi Shanker <ravi.m.shanker@gmail.com>
2012-07-30 10:47:26 -0700

    initialized a new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pollster::Application.config.secret_token = 'a065b6e35016fbee86842d5721f7b1bc7d194348c25a8a3ed93bb57df5c2429e407d274013638da35310600e6a87b813dc89b84369945091ccc11871a083f42d'
